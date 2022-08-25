2d431281cde7ae0e5b80dc638b34e1ae559aecae
shivamkhatkale <shivamkhatkale5@gmail.com>
2022-08-25 22:56:01 +0530

    first commit

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from .models import Product
+from math import ceil
 
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products= Product.objects.all()
+    n= len(products)
+    nSlides= n//4 + ceil((n/4) + (n//4))
+    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    return render(request,"shop/index.html", params)
 
 def about(request):
     return render(request, 'shop/about.html')
