a09e729cbe46c0fe65b375cea192118414d55de4
shivamkhatkale <shivamkhatkale5@gmail.com>
2022-09-07 10:56:09 +0530

    first commit

--- mac/blog/urls.py
+++ mac/blog/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from .import views
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path("", views.index, name="ShopName"),
+    path('', views.index,name="BlogHome"),
+    path('blogpost/', views.blogpost,name="blogPost"),
 ]
